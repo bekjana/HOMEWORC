f0fa315a65dd7d6a08b4c6f3674b18a241a122c5
bekjana <127587694+bekjana@users.noreply.github.com>
2023-03-11 13:46:50 +0600

    сделал дз2

---
+++ lesson2/main.js
@@ -0,0 +1,44 @@
+// var insert = prompt('one or zwei')
+//
+// var obj = {
+//     russian
+// }
+//
+// var one = ['tuii','hfv']
+// var zwei = ['suka','kbhjgh']
+//
+// switch (insert){
+//     case 'one':
+//         console.log(one)
+//         break
+//     case'zwei':
+//         console.log(zwei)
+//         break
+//     default:
+//         alert('ERROR')
+//         break
+// }
+// var number = 0
+// number = 10
+// number ++
+// number =number + 1
+
+// console.log(number)
+// var arr = ['a','b','c','d']
+// console.log(arr)
+// arr.push('e')
+// arr.unshift('e')
+// arr.pop()
+// arr.shift()
+// arr.splice('0','1')
+// console.log(arr)
+//
+// var newArr = arr.slice(0,1)
+////////////////////////////////////////////////////
+// let arr = [10, 20, 30, 50, 235, 3000];
+// console.log(arr.filter(num => ['1','2','5'].includes(num.toString()[0])));
+
+// for (var icom = 20; icom > 0; icom = icom - 1){
+//     console.log(icom)
+// }
+var 
